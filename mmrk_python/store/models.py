a2d78f8fdf8126499154a31d14238a15206294a3
replacementorange <notnenar@proton.me>
2025-02-11 22:38:21 +0200

    Created db models

--- mmrk_python/store/models.py
+++ mmrk_python/store/models.py
@@ -1,3 +1,61 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# Customer model
+class Customer(models.Model):
+    """Each customer model holds one to one relatioship to each user."""
+    user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+# Product model
+class Product(models.Model):
+    """Represents store product. Parent of OrderItem."""
+    name = models.CharField(max_length=80)
+    price = models.FloatField()
+    digital = models.BooleanField(default=False, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+# Order model
+class Order(models.Model):
+    """Represents the transaction that is placed or pending. 
+    Holds transaction's ID, order status. Parent of OrderItem."""
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True) 
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False)
+    transaction_id = models.CharField(max_length=50, null=True)
+
+    def __str__(self):
+        return str(self.id)
+
+
+# Order's item model
+class OrderItem(models.Model):
+    """Represents order's single item. Child of the product and order models."""
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True, blank=True)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+
+# Shipping model
+class ShippingAddress(models.Model):
+    """Represents order's shipping information."""
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    address = models.CharField(max_length=50, null=False)
+    city = models.CharField(max_length=50, null=False)
+    state = models.CharField(max_length=50, null=False)
+    zipcode = models.CharField(max_length=10, null=False)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.address
