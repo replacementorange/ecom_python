eb315462b0e42372755b6c4efb6a400005e46158
replacementorange <notnenar@proton.me>
2025-02-15 12:46:09 +0200

    Add to cart

---
+++ mmrk_python/static/js/cart.js
@@ -0,0 +1,38 @@
+var updateButtons = document.getElementsByClassName("update-cart")
+
+for (i = 0; i < updateButtons.length; i++) {
+    updateButtons[i].addEventListener("click", function(){
+        var productId = this.dataset.product
+        var action = this.dataset.action
+        console.log("ProductId: ", productId, "Action: ", action)
+
+        console.log("USER: ", user)
+        if (user == "AnymousUser") {
+            console.log("User is not logged in.")
+        } else {
+            updateUserOrder(productId, action)
+        }
+    })
+}
+
+function updateUserOrder(productId, action) {
+    console.log("User is logged in: sending data.")
+
+    var url = "/update_item/"
+
+    fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type":"application/json",
+            "X-CSRFToken":csrftoken,
+        },
+        body:JSON.stringify({"productId":productId, "action":action})
+    })
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        //console.log("Data:", data)
+        location.reload()
+    }) 
+}
--- mmrk_python/store/views.py
+++ mmrk_python/store/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from .models import Customer, Product, Order, OrderItem, ShippingAddress
+
+import json
 
 
 # Store page view
 def store(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+    else:
+        # Create empty cart for none-logged in users
+        items = []
+        order = {"get_cart_total": 0, "get_cart_items": 0}
+        cartItems = order["get_cart_items"]
+
     products = Product.objects.all()
-    context = {'products': products}
+    context = {"products": products, "cartItems":cartItems}
     return render(request, "store/store.html", context)
 
 
@@ -15,12 +29,14 @@ def cart(request):
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
     else:
         # Create empty cart for none-logged in users
         items = []
-        order = {'get_cart_total': 0, 'get_cart_items': 0}
+        order = {"get_cart_total": 0, "get_cart_items": 0}
+        cartItems = order["get_cart_items"]
 
-    context = {'items': items, 'order': order}
+    context = {"items": items, "order": order, "cartItems":cartItems}
     return render(request, "store/cart.html", context)
 
 
@@ -30,10 +46,39 @@ def checkout(request):
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
     else:
         # Create empty cart for none-logged in users
         items = []
-        order = {'get_cart_total': 0, 'get_cart_items': 0}
+        order = {"get_cart_total": 0, "get_cart_items": 0}
+        cartItems = order["get_cart_items"]
 
-    context = {'items': items, 'order': order}
+    context = {"items": items, "order": order, "cartItems":cartItems}
     return render(request, "store/checkout.html", context)
+
+
+# Updates item in cart
+def updateItem(request):
+    data = json.loads(request.body)
+    productId = data["productId"]
+    action = data["action"]
+    print("Action", action)
+    print("Product", productId)
+
+    customer = request.user.customer
+    product = Product.objects.get(id=productId)
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+    orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+    if action == "add":
+        orderItem.quantity = (orderItem.quantity + 1)
+    elif action == "remove":
+        orderItem.quantity = (orderItem.quantity - 1)
+    
+    orderItem.save()
+
+    if orderItem.quantity <= 0:
+        orderItem.delete()
+
+    return JsonResponse("Item was added", safe=False)
